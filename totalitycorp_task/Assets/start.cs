4a17e95356730faeb96665b3694d648a64a2b506
Saikarthi <41743913+Saikarthi@users.noreply.github.com>
2020-10-27 00:34:13 +0530

    task

---
+++ totalitycorp_task/Assets/start.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class start : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //public FlexibleColorPicker f;
+    public ColorPickerTriangle c;
+    public Camera cam;
+    public manger man;
+    private bool t=false;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+           cam.backgroundColor = c.TheColor;
+        
+    }
+   public void get_in()
+    {
+        man.enabled = true;
+    }
+    public void menu()
+    {
+        
+        man.enabled = false;
+    }
+
+    public void bgclick()
+    {
+        man.enabled = false;
+    }
+    public void back()
+    {
+        man.enabled = true;
+
+    }
+}
